86d958b89fe60655a0a6fd33e1f731d7f3073578
RealTim0 <gemtimo994@gmail.com>
2023-06-23 14:27:16 +0400

    A simple react info site created using react and vite showing important facts about React

---
+++ react-airbnb/src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react"
+import Main from "./Body.jsx"
+import Navbar from "./Navbar.jsx"
+
+export default function (){
+  return(
+    <>
+      <Navbar/>
+      <Main />
+    </>
+  )
+}
